65ab4021dae5f80376cf0404afd2ec345490d1c1
Owinogoddie <112401055+Owinogoddie@users.noreply.github.com>
2023-03-21 07:56:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
 const express = require('express');
-
-const mongoose=require('mongoose')
 const cors=require('cors')
 const app = express();
 app.use(express.json())
 app.use(cors())
 
 
+app.post('/api/register',(req,res)=>{
+  data=req.body
+  res.json({data:data})
+})
 
-
-app.get('/api/works', (req, res) => {
+app.get('/', (req, res) => {
 
     res.json({message:'Hello World!'
 });
   });
 
 
-// mongoose.connect('mongodb://localhost:27017/netninja-crud')
-mongoose.connect("mongodb+srv://Owinogoddie:goddythedevninja@cluster0.ywulxup.mongodb.net/?retryWrites=true&w=majority")
-.then(()=>{  
-
-app.listen(process.env.PORT,()=>{
-    console.log(`connected to db and app running on ${process.env.PORT}`)
-})
-
-})
-.catch((error)=>{
-    console.log(error)
-})
-
-export default app;
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+  });
