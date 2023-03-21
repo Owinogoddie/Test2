70ed4629faad6b5a37e2783be6790e655989ce5a
Owinogoddie <112401055+Owinogoddie@users.noreply.github.com>
2023-03-21 07:54:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,19 @@
-const express=require('express')
+const express = require('express');
+
 const mongoose=require('mongoose')
-require('dotenv').config()
 const cors=require('cors')
-const workoutsRoutes=require('./routes/workouts')
-const annotationRoutes=require('./routes/annotationRoutes')
-const expertiseRoutes=require('./routes/expertiseRoutes')
-const useCaseCategoryRoutes=require('./routes/useCaseCategoryRoutes')
-const useCaseRoutes=require('./routes/useCaseRoutes')
-
-const visionCatRoutes=require('./routes/visionCatRoutes')
-const benefitsRoutes=require('./routes/benefitsRoutes')
-
-
-// const visionRoutes=require('./routes/visionRoutes')
-
-const allVisionsRoutes=require('./routes/allVisionRoutes')
-
-
-
-const app=express();
-const bodyParser = require('body-parser');
-
-
-//middleware
-
+const app = express();
+app.use(express.json())
 app.use(cors())
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-app.use('/api/workouts',workoutsRoutes)
-app.use('/api/annotations',annotationRoutes)
-app.use('/api/expertise',expertiseRoutes)
-app.use('/api/casecategory',useCaseCategoryRoutes)
-app.use('/api/usecases',useCaseRoutes)
 
-app.use('/api/visioncats',visionCatRoutes)
-// app.use('/api/visions',visionRoutes)
-app.use('/api/allvisions',allVisionsRoutes)
-app.use('/api/benefits',benefitsRoutes)
+
+
+app.get('/api/works', (req, res) => {
+
+    res.json({message:'Hello World!'
+});
+  });
 
 
 // mongoose.connect('mongodb://localhost:27017/netninja-crud')
