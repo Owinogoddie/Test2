90b07686df3cbc659e389f48c2681f2ab93dfc18
Owinogoddie <112401055+Owinogoddie@users.noreply.github.com>
2023-03-21 08:05:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose=require('mongoose')
+const bodyParser = require('body-parser');
+
+const annotationRoutes=require('./routes/annotationRoutes')
+
 const cors=require('cors')
 const app = express();
 app.use(express.json())
 app.use(cors())
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
 app.get('/api/register',(req,res)=>{
@@ -16,6 +22,11 @@ app.get('/', (req, res) => {
     res.json({message:'Hello World!'
 });
   });
+
+
+
+
+app.use('/api/annotations',annotationRoutes)
 
 mongoose.connect("mongodb+srv://Owinogoddie:goddythedevninja@cluster0.ywulxup.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{  
